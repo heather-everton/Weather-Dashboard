8fc870a51f5ffe1e764e9ee2c300be6568360368
Heather Everton <heather.everton@planplusonline.com>
2020-09-06 18:32:12 -0600

    add 5 day forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,65 @@
+$(document).ready(function(){
+
 var weatherFormEl = document.querySelector("#weather-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
 var zipSearchTerm = document.querySelector("#zip-search-term");
-
-
+var cities = JSON.parse(localStorage.getItem ("cities")) || [];
+var lat;
+var lon;
+    
 
 var getWeatherInfo = function(city) {
     // format the github api url
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ceb07fd28e874e56f65ba9fc8499c396";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayWeather(data, city);
-            });
-          } else {
-            alert("Error: " + response.statusText);
-          }
-    })
-    .catch(function(error) {
-        // Notice this ".catch()" getting chained onto the end of the ".then()" method
-        alert("Unable to connect to OpenWeather")
-    })
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json()
+                .then(function(data) {
+                    displayWeather(data, city)
+                    return data
+                })
+                .then(function(data){
+                    var lat = data.coord.lat 
+                    var lon = data.coord.lon
+                    return fetch(
+                        "http://api.openweathermap.org/data/2.5/uvi?appid=ceb07fd28e874e56f65ba9fc8499c396&lat=" + lat + "&lon=" + lon 
+                    )
+                })
+
+                .then (function(response) {
+                    // console.log (response.json())
+                    return response.json()
+                })
+                .then(function(data){
+                    var uvIndex = data.value;
+                    // create contaiiner for the weather info
+                    var uvEl = document.createElement("div");
+             
+                    //create a span element to hold city name
+                    var weatherEl = document.createElement("span");
+                    weatherEl.textContent = "UV Index: " + uvIndex;
+                
+                    //append to container
+                    uvEl.appendChild(weatherEl);
+                    
+                    // append container to the dom
+                    weatherContainerEl.appendChild(uvEl)
+                });
+                
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            // Notice this ".catch()" getting chained onto the end of the ".then()" method
+            alert("Unable to connect to OpenWeather")
+        })
+
+        getForecast(city);
 };
 
 var formSubmitHandler = function(event) {
@@ -31,10 +68,12 @@ var formSubmitHandler = function(event) {
 
     if (cityName) {
         getWeatherInfo(cityName);
+        displayHistory(cityName)
         cityInputEl.value = "";
     } else {
         alert("Please enter a city");
     }
+    saveHistory(cityName);
 };
 
 var displayWeather = function(response) {
@@ -48,40 +87,109 @@ var displayWeather = function(response) {
     weatherContainerEl.textContent = "";
     // zipSearchTerm.textContent = searchTerm;
 
-    var cityName = response.name;
-
-    // create contaiiner for the weather info
-    var cityEl = document.createElement("h1");
-    
-    // cityEl.classList = "list-item flex-row justify-space-between align-center";
-
-    //create a span element to hold city name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = cityName;
-
-    //append to container
-    cityEl.appendChild(titleEl);
-    
-    // append container to the dom
-    weatherContainerEl.appendChild(cityEl)
-
-    for (property in response.main){
-        var weatherInfo = property +" "+ response.main[property];
-
+    var cityName = response.name + " " + moment().format("(MM[/]DD[/]YYYY)");
         // create contaiiner for the weather info
-        var weatherEl = document.createElement("div");
-            
+        var cityEl = document.createElement("h1");
+    
         //create a span element to hold city name
-        var tempEl = document.createElement("span");
-        tempEl.textContent = weatherInfo;
+        var titleEl = document.createElement("span");
+        titleEl.textContent = cityName;
     
         //append to container
-        weatherEl.appendChild(tempEl);
+        cityEl.appendChild(titleEl);
         
         // append container to the dom
-        weatherContainerEl.appendChild(weatherEl)
-    }
+        weatherContainerEl.appendChild(cityEl)
+
+    var temp = response.main.temp
+        // create contaiiner for the weather info
+        var tempEl = document.createElement("div");
+    
+        //create a span element to hold city name
+        var weatherEl = document.createElement("span");
+        weatherEl.textContent = "Current Temp: " + temp + "F";
+    
+        //append to container
+        tempEl.appendChild(weatherEl);
+        
+        // append container to the dom
+        weatherContainerEl.appendChild(tempEl)
+    
+    var humidity = response.main.humidity
+        // create contaiiner for the weather info
+        var humidityEl = document.createElement("div");
+    
+        //create a span element to hold city name
+        var weatherEl = document.createElement("span");
+        weatherEl.textContent = "Humidity: " + humidity + "%";
+        
+        //append to container
+        humidityEl.appendChild(weatherEl);
+            
+        // append container to the dom
+        weatherContainerEl.appendChild(humidityEl)
+
+    var wind = response.wind.speed
+    var windEl = document.createElement("div");
+    var windCont = document.createElement("span");
+    windCont.textContent = "Wind speed: " + wind + "MPH";
+    windEl.appendChild(windCont);
+    weatherContainerEl.appendChild(windEl)
+    weatherContainerEl.appendChild(windEl, humidityEl, tempEl)
 
 };
-  
+
+var saveHistory = function (cityName) {
+    cities.push(cityName);
+    localStorage.setItem ("cities", JSON.stringify(cities))
+};
+
+var displayHistory = function (cityName) {
+    for (i = 0; i < cities.length; i++) {
+        // var citiesArr = cities[i]
+        var cityButton = $("button").text(cities[i])
+        var historyEl = $("#search-history");
+        historyEl.append(cityButton);
+    };
+}
+
+var getForecast = function(city){  
+    // make a request to the url
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=ceb07fd28e874e56f65ba9fc8499c396")
+    .then(function(response) {
+        response.json() 
+        .then(function(data) {
+            for (i = 0; i < data.list.length; i++) {
+                if (data.list[i].dt_txt.indexOf("15:00:00") !== -1){
+                    //create cards for weather info
+                    var forecastInfo = $("#forecast")
+                    var dailyCol = $("<div>").addClass("col-md-2")
+                    var dailyCard = $("<div>").addClass("card")
+                    var cardBody = $("<div>").addClass("card-body")
+
+                    //create data to go on the cards
+                    var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                    var cardTemp = $("<p>").text("Temp: " + data.list[i].main.temp_max)
+                    var cardHumidity = $("<p>").text("Humidity: " + data.list[i].main.humidity)
+                    
+                    //put the data in the cardBody
+                    cardBody.append(img);
+                    cardBody.append(cardTemp);
+                    cardBody.append(cardHumidity);
+
+                    //put the card body in the columns
+                    dailyCard.append(cardBody);
+                    dailyCol.append(dailyCard);
+                    forecastInfo.append(dailyCol);
+                    
+                }
+
+            }
+        })
+    });
+}
+
+
 weatherFormEl.addEventListener("submit", formSubmitHandler);
+displayHistory ()
+})
